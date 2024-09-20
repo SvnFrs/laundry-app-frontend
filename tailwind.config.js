1c8f6f5831b3be2385afe6f3b62b258c4acecf35
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-09-20 17:41:22 +0700

    Update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sky": {
+          "50": "#f0fafa",
+          "100": "#e0f4f5",
+          "200": "#c1e9eb",
+          "300": "#a3dee2",
+          "400": "#84d3d8",
+          "500": "#65c8ce",
+          "600": "#51a0a5",
+          "700": "#3d787c",
+          "800": "#285052",
+          "900": "#142829"
+        }
+      }
+    }
   },
   plugins: [],
 }
