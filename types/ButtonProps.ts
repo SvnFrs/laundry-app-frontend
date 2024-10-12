d4c027174f587e257bd708d9e288c8d01ffe6b40
SvnFrs <thaidvq.work@gmail.com>
2024-10-13 06:50:27 +0700

    update types

--- types/ButtonProps.ts
+++ types/ButtonProps.ts
@@ -1,9 +1,10 @@
 export interface ButtonProps {
-    destination?: any;
+    destination?: any; // Change to string for href compatibility
     label?: string;
     width?: number;
     flex?: number;
     icon?: any;
+    color: string;
     iconColor?: string;
     buttonColor?: string;
     textColor?: string;
@@ -12,4 +13,5 @@ export interface ButtonProps {
     selected?: boolean;
     onPress?: () => void;
     additionalInfo?: string;
+    disabled?: boolean; // Add disabled prop
 }
