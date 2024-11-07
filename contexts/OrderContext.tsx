484f72b96f6dd9c14e50446b5a5c3aac03e60c49
SvnFrs <thaidvq.work@gmail.com>
2024-11-08 05:10:16 +0700

    update context

--- contexts/OrderContext.tsx
+++ contexts/OrderContext.tsx
@@ -1,21 +1,29 @@
 import React, { createContext, useState, useContext, ReactNode } from 'react';
 
 interface OrderContextType {
-  orderId: string | null;
-  setOrderId: (id: string | null) => void;
+  orderIds: string[]; // Store multiple order IDs
+  addOrderId: (id: string) => void; // Append a new order ID
   progress: number;
   setProgress: (progress: number) => void;
+  isComplete: boolean;
+  setIsComplete: (isComplete: boolean) => void;
 }
 
 // Create context
 const OrderContext = createContext<OrderContextType | undefined>(undefined);
 
 export const OrderProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [orderId, setOrderId] = useState<string | null>(null);
+  const [orderIds, setOrderIds] = useState<string[]>([]); // Initialize as an empty array
   const [progress, setProgress] = useState<number>(100);
+  const [isComplete, setIsComplete] = useState(false);
+
+  // Function to add a new order ID
+  const addOrderId = (id: string) => {
+    setOrderIds((prevOrderIds) => [...prevOrderIds, id]);
+  };
 
   return (
-    <OrderContext.Provider value={{ orderId, setOrderId, progress, setProgress }}>
+    <OrderContext.Provider value={{ orderIds, addOrderId, progress, setProgress, isComplete, setIsComplete }}>
       {children}
     </OrderContext.Provider>
   );
